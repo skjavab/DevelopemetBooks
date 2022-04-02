6b9781caca815945592542cb30212f79803c39ca
skjavab <srshryls@gamil.com>
2022-04-02 22:26:10 +0530

    Calculate price should return 95 when user buys Two different book

--- src/main/java/com/bnp/kata/developemetbooks/services/BookPurchaseService.java
+++ src/main/java/com/bnp/kata/developemetbooks/services/BookPurchaseService.java
@@ -1,13 +1,25 @@
 package com.bnp.kata.developemetbooks.services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.bnp.kata.developemetbooks.model.ShoppingCartItem;
 
 @Service
 public class BookPurchaseService {
-	public Double calculateBooksPrice(ShoppingCartItem shoppingCartItem) {
-		return 50d * shoppingCartItem.getQuantity();
+	public Double calculateBooksPrice(List<ShoppingCartItem> bookList) {
+		double calculatedPrice = 0d;
+		double price = 50d;
+		if (bookList.size() == 1) {
+			ShoppingCartItem shoppingCartItembook = bookList.get(0);
+			return price * shoppingCartItembook.getQuantity();
+		} else if (bookList.size() == 2) {
+			double basePrice = bookList.size() * price;
+			double discountedPriceForSet = basePrice - ((basePrice * 5) / 100.0);
+			calculatedPrice += discountedPriceForSet;
+		}
+		return calculatedPrice;
 
 	}
 
--- src/test/java/com/bnp/kata/developemetbooks/services/BookPurchaseServicesTest.java
+++ src/test/java/com/bnp/kata/developemetbooks/services/BookPurchaseServicesTest.java
@@ -1,5 +1,8 @@
 package com.bnp.kata.developemetbooks.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -22,18 +25,37 @@ public class BookPurchaseServicesTest {
 	@Test
 	public void calculatePriceShouldReturn50WhenUserBuysOneBook() {
 		bookPurchaseService = new BookPurchaseService();
+		List<ShoppingCartItem> bookList = new ArrayList<>();
 		setShoppingCart(1, 1);
+		bookList.add(shoppingCartItem);
 
-		Assertions.assertEquals(50d, bookPurchaseService.calculateBooksPrice(shoppingCartItem));
+		Assertions.assertEquals(50d, bookPurchaseService.calculateBooksPrice(bookList));
 
 	}
 
 	@Test
 	public void calculatePriceShouldReturn100WhenUserBuysTwoBook() {
 		bookPurchaseService = new BookPurchaseService();
+		List<ShoppingCartItem> bookList = new ArrayList<>();
 		setShoppingCart(1, 2);
+		bookList.add(shoppingCartItem);
 
-		Assertions.assertEquals(100d, bookPurchaseService.calculateBooksPrice(shoppingCartItem));
+		Assertions.assertEquals(100d, bookPurchaseService.calculateBooksPrice(bookList));
+
+	}
+
+	@Test
+	public void calculatePriceShouldReturn95WhenUserBuysTwoDifferentBook() {
+		bookPurchaseService = new BookPurchaseService();
+		List<ShoppingCartItem> bookList = new ArrayList<>();
+		setShoppingCart(1, 1);
+		setShoppingCart(1, 2);
+		bookList.add(shoppingCartItem);
+		setShoppingCart(2, 1);
+		setShoppingCart(1, 2);
+		bookList.add(shoppingCartItem);
+
+		Assertions.assertEquals(95d, bookPurchaseService.calculateBooksPrice(bookList));
 
 	}
 
