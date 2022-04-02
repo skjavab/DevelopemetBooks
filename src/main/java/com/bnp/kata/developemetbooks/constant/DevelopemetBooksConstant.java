aac9c183bce61f341b8995f1fcc104cf9300f1cd
skjavab <srshryls@gamil.com>
2022-04-02 22:48:17 +0530

    Code refactoring :add clean

---
+++ src/main/java/com/bnp/kata/developemetbooks/constant/DevelopemetBooksConstant.java
@@ -0,0 +1,8 @@
+package com.bnp.kata.developemetbooks.constant;
+
+public class DevelopemetBooksConstant {
+	
+	public static final double PRICE =50d;
+	public static final double PERCENTAGE =100.0d;
+
+}
--- src/main/java/com/bnp/kata/developemetbooks/services/BookPurchaseService.java
+++ src/main/java/com/bnp/kata/developemetbooks/services/BookPurchaseService.java
@@ -4,23 +4,24 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.bnp.kata.developemetbooks.constant.DevelopemetBooksConstant;
 import com.bnp.kata.developemetbooks.model.ShoppingCartItem;
 
 @Service
 public class BookPurchaseService {
 	public Double calculateBooksPrice(List<ShoppingCartItem> bookList) {
 		double calculatedPrice = 0d;
-		double price = 50d;
+		// double price = 50d;
 		if (bookList.size() == 1) {
 			ShoppingCartItem shoppingCartItembook = bookList.get(0);
-			return price * shoppingCartItembook.getQuantity();
+			calculatedPrice = DevelopemetBooksConstant.PRICE * shoppingCartItembook.getQuantity();
 		} else if (bookList.size() == 2) {
-			double basePrice = bookList.size() * price;
-			double discountedPriceForSet = basePrice - ((basePrice * 5) / 100.0);
+			double basePrice = bookList.size() * DevelopemetBooksConstant.PRICE;
+			double discountedPriceForSet = basePrice - ((basePrice * 5) / DevelopemetBooksConstant.PERCENTAGE);
 			calculatedPrice += discountedPriceForSet;
 		} else if (bookList.size() == 3) {
-			double basePrice = bookList.size() * price;
-			double discountedPriceForSet = basePrice - ((basePrice * 10) / 100.0);
+			double basePrice = bookList.size() * DevelopemetBooksConstant.PRICE;
+			double discountedPriceForSet = basePrice - ((basePrice * 10) / DevelopemetBooksConstant.PERCENTAGE);
 			calculatedPrice += discountedPriceForSet;
 		}
 		return calculatedPrice;
