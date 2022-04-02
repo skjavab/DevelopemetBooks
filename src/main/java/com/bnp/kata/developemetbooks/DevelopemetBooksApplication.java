8da6ce030ee160a921fe1c21cbda8113ebfe18bb
skjavab <srshryls@gamil.com>
2022-04-02 21:13:06 +0530

    Created spring boot application and add dependencies for developing the project

---
+++ src/main/java/com/bnp/kata/developemetbooks/DevelopemetBooksApplication.java
@@ -0,0 +1,13 @@
+package com.bnp.kata.developemetbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopemetBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopemetBooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bnp/kata/developemetbooks/DevelopemetBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnp.kata.developemetbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopemetBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
